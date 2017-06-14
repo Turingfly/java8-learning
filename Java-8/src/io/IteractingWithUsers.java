c87edc800b16b120884701b5c1177cafebcabe85
turingfly <chengfei@udel.edu>
2017-06-14 15:36:46 -0400

    IO

---
+++ Java-8/src/io/IteractingWithUsers.java
@@ -0,0 +1,77 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * 
+ * @author chengfeili 
+ * Jun 14, 2017 3:18:58 PM
+ *
+ *         The Console class is a singleton.
+ * 
+ *         This is a bug #122429 of eclipse
+ * 
+ */
+public class IteractingWithUsers {
+
+	public void readLineTest() throws IOException {
+		Console console = System.console();
+		if (console == null) {
+			throw new RuntimeException("Console not available");
+		} else {
+			console.writer().print("How excited are you about your trip today? ");
+			console.flush();
+			String excitementAnswer = console.readLine();
+			String name = console.readLine("Please enter your name: ");
+			Integer age = null;
+			console.writer().print("What is your age? ");
+			console.flush();
+			BufferedReader reader = new BufferedReader(console.reader());
+			String value = reader.readLine();
+			age = Integer.valueOf(value);
+			console.writer().println();
+			console.format("Your name is " + name);
+			console.writer().println();
+			console.format("Your age is " + age);
+			console.printf("Your excitement level is: " + excitementAnswer);
+		}
+	}
+
+	/**
+	 * By disabling echoing, the user does not see the text they are typing.
+	 * 
+	 * The readPassword() method returns an array of characters instead of a
+	 * String. String values are added to a shared memory pool for performance
+	 * reasons in Java. This means that if a password that a user typed in were
+	 * to be returned to the process as a String , it might be available in the
+	 * String pool long after the user entered it.
+	 */
+	public void readPasswordTest() {
+		Console console = System.console();
+		if (console == null) {
+			throw new RuntimeException("Console not available");
+		} else {
+			char[] password = console.readPassword("Enter your password: ");
+			console.format("Enter your password again: ");
+			console.flush();
+			char[] verify = console.readPassword();
+			boolean match = Arrays.equals(password, verify);
+			// Immediately clear passwords from memory
+			// Arrays.fill(password, 'x');
+			for (int i = 0; i < password.length; i++) {
+				password[i] = 'x';
+			}
+			for (int i = 0; i < verify.length; i++) {
+				verify[i] = 'x';
+			}
+			console.format("Your password was " + (match ? "correct" : "incorrect"));
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
--- Java-8/src/io/StreamsContinue.java
+++ Java-8/src/io/StreamsContinue.java
@@ -1,6 +1,21 @@
 package io;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +31,7 @@ public class StreamsContinue {
 	public void fileInputOutputStream() throws IOException {
 		File source = new File("Zoo.class");
 		File destination = new File("ZooCopy.class");
-		try (InputStream in = new FileInputStream(source); 
-				OutputStream out = new FileOutputStream(destination)) {
+		try (InputStream in = new FileInputStream(source); OutputStream out = new FileOutputStream(destination)) {
 			int b;
 			while ((b = in.read()) != -1) {
 				out.write(b);
@@ -59,7 +73,7 @@ public class StreamsContinue {
 	}
 
 	// BufferedReader
-	public static List<String> readFile(File source) throws IOException {
+	public List<String> readFile(File source) throws IOException {
 		List<String> data = new ArrayList<String>();
 		try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
 			String s;
@@ -71,7 +85,7 @@ public class StreamsContinue {
 	}
 
 	// BufferedWriter
-	public static void writeFile(List<String> data, File destination) throws IOException {
+	public void writeFile(List<String> data, File destination) throws IOException {
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(destination))) {
 			for (String s : data) {
 				writer.write(s);
@@ -79,7 +93,111 @@ public class StreamsContinue {
 		}
 	}
 
+	// Deserialize
+	public List<Animal> getAnimals(File dataFile) throws IOException, ClassNotFoundException {
+		List<Animal> animals = new ArrayList<Animal>();
+		try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(dataFile)))) {
+			while (true) {
+				Object object = in.readObject();
+				if (object instanceof Animal)
+					animals.add((Animal) object);
+			}
+		} catch (EOFException e) {
+			// File end reached
+		}
+		return animals;
+	}
+
+	// Serialize
+	public static void createAnimalsFile(List<Animal> animals, File dataFile) throws IOException {
+		try (ObjectOutputStream out = new ObjectOutputStream(
+				new BufferedOutputStream(new FileOutputStream(dataFile)))) {
+			for (Animal animal : animals)
+				out.writeObject(animal);
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 
 	}
+}
+
+/**
+ * Any class can implement the Serializable interface since there are no
+ * required methods to implement.
+ * 
+ * Note that the requirement for properly marking an object as Serializable may
+ * involve nested objects.
+ * 
+ * Therefore, any object references contained within the Tail class must belong
+ * to classes that are also marked as Serializable , and so on.
+ * 
+ * Besides transient instance variables, static class members will also be
+ * ignored during the serialization and deserialization process.
+ * 
+ * Why Not Mark Every Class as Serializable ?
+ * 
+ * The reason that we do not is that there are some classes that we want to
+ * instruct the JVM not to serialize. In particular, process-heavy classes such
+ * as the Thread class or any of the Stream classes would be diffi cult, often
+ * impossible, to save to persistent storage, since much of their work involves
+ * managing JVM processes or resources in real time.
+ *
+ */
+class Animal implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private int age;
+	private char type;
+
+	public Animal(String name, int age, char type) {
+		this.name = name;
+		this.age = age;
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public char getType() {
+		return type;
+	}
+
+	public String toString() {
+		return "Animal [name=" + name + ", age=" + age + ", type=" + type + "]";
+	}
+}
+
+class Animal1 implements Serializable {
+	private static final long serialVersionUID = 2L;
+	private transient String name;
+	private transient int age = 10;
+	private static char type = 'C';
+	{
+		this.age = 14;
+	}
+
+	public Animal1() {
+		this.name = "Unknown";
+		this.age = 12;
+		this.type = 'Q';
+	}
+
+	public Animal1(String name, int age, char type) {
+		this.name = name;
+		this.age = age;
+		this.type = type;
+	}
+	// Same methods as before
+
+	// [Animal [name=null, age=0, type=P], Animal [name=null, age=0, type=P]]
+	/*
+	 * As expected, you can see that the values for name and age are lost on
+	 * serialization and not set again during deserialization.
+	 */
 }
